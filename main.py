c8de7d9290d0801983979897adcd6065061fa5a2
Ak123yay <aarushkatam5@gmail.com>
2025-07-13 13:46:30 -0400

    hi

--- main.py
+++ main.py
@@ -9,3 +9,16 @@ class PasswordManager:
         chars = string.ascii_letters + string.digits + "!@#$%^&*()"
         return "".join(random.choice(chars) for _ in range(length))
 
+    def add_password(self, site, username, password):
+        if site in self.passwords:
+            print(f"Password already exists. Saved new password.")
+        self.passwords[site] = {"username": username, "password": password}
+        print(f"Password saved.")
+
+    def get_password(self, site):
+        if site in self.passwords:
+            info = self.passwords[site]
+            print(f"Site: {site}\nUsername: {info['username']}\nPassword: {info['password']}")
+        else:
+            print("No password found.")
+
