abe24c8c646f3ffaae79507dcb4cddee14fa2c3c
Ak123yay <aarushkatam5@gmail.com>
2025-07-13 13:36:12 -0400

    hi

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ class PasswordManager:
     def __init__(self):
         self.passwords = {}
 
+    def generate_password(self, length=12):
+        chars = string.ascii_letters + string.digits + "!@#$%^&*()"
+        return "".join(random.choice(chars) for _ in range(length))
+
